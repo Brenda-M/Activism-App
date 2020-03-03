b739806dc7ee8d9a2cbe56d911d499b5c7d6fbb4
Brenda-M <brendanjuguna1@gmail.com>
2020-03-03 15:34:34 +0300

    add data for homepage in mockdat

--- js/index.js
+++ js/index.js
@@ -1,7 +1,36 @@
-const description = `
-  <p>I am a description</p>
-`;
+
+const Movements = [
+  {
+    title: "Power to the period",
+    description: "Collect and donate period products for people experience poverty.",
+    detailedDescription: "",
+    image:  "assets/recents1.jpg" 
+  },
+  {
+    title: "My mental health",
+    description: "On trauma and depression, resilience and strength...",
+    image: "assets/recents2.jpeg"
+  },
+  {
+    title: "Tackel Hunger",
+    description: "Collect food items for your local food bank",
+    image: "assets/recents3.jpeg"
+  },
+];
 
 $(document).ready(function () {
-  
+  const recentsDiv = $("#recentStories");
+  console.log(Movements)
+  for (let i = 0; i < Movements.length; i++) {
+    recentsDiv.append(`
+      <div class="card" style="width: 18rem;">
+        <img src="${Movements[i].image}" class="card-img-top" alt="${Movements[i].title}">
+        <div class="card-body">
+          <h5 class="card-title">${Movements[i].title}</h5>
+          <p class="card-text">${Movements[i].description}</p>
+          <a href="#" class="btn btn-primary">See More</a>
+        </div>
+      </div>
+    `);
+  }
 });
---
+++ js/mock-data.js
@@ -0,0 +1,20 @@
+const Movements = [
+    {
+        title: "Power to the period",
+        description:"Collect and donate period products for people experience poverty.",
+        image:"/assests/recents1.jpg"
+    }, 
+    {
+        title: "",
+        description: "",
+        image: ""
+    },
+    {
+        title: "",
+        description: "",
+        image: ""
+    },
+];
+
+
+
