ec730c9345c3363c25bcd289babceac6026dc3bd
Brenda-M <brendanjuguna1@gmail.com>
2020-03-03 17:33:33 +0300

    add 9 stories to the homepage

--- js/index.js
+++ js/index.js
@@ -2,7 +2,7 @@
 const Movements = [
   {
     title: "Power to the period",
-    description: "Collect and donate period products for people experience poverty.",
+    description: "Collect and donate sanitary products for people experience poverty.",
     detailedDescription: "",
     image:  "assets/recents1.jpg" 
   },
@@ -17,36 +17,36 @@ const Movements = [
     image: "assets/recents3.jpeg"
   },
   {
-    title: "No to ha",
-    description: "Collect and donate period products for people experience poverty.",
+    title: "No to hate",
+    description: "Respond to tribal hate with action and love.",
     detailedDescription: "",
-    image: "assets/recents1.jpg"
+    image: "assets/recents4.jpeg"
   },
   {
-    title: "My mental health",
-    description: "On trauma and depression, resilience and strength...",
-    image: "assets/recents2.jpeg"
+    title: "Elephant Tusk Force",
+    description: "Flag ivory items on the blackmarket to stop elephant poaching...",
+    image: "assets/recents5.jpeg"
   },
   {
-    title: "Tackel Hunger",
-    description: "Collect food items for your local food bank",
-    image: "assets/recents3.jpeg"
+    title: "Ending the climate crisis",
+    description: "We are in a climate crisis emergency and the oil, gas, and coal indutry is to blame..",
+    image: "assets/recents6.jpeg"
   },
   {
-    title: "Power to the period",
-    description: "Collect and donate period products for people experience poverty.",
+    title: "Protecting the oceans",
+    description: "Our oceans are in more trouble than ever before.",
     detailedDescription: "",
-    image: "assets/recents1.jpg"
+    image: "assets/recents7.jpeg"
   },
   {
-    title: "My mental health",
-    description: "On trauma and depression, resilience and strength...",
-    image: "assets/recents2.jpeg"
+    title: "Defending Democracy",
+    description: "We believe that a healthy democracy is the precondition to a healthy environment...",
+    image: "assets/recents8.jpeg"
   },
   {
-    title: "Tackel Hunger",
-    description: "Collect food items for your local food bank",
-    image: "assets/recents3.jpeg"
+    title: "Kick corporate money out of politics",
+    description: "It may seem like corporations have had a firm grip on Kenyan politics for generations",
+    image: "assets/recents9.jpeg"
   },
 ];
 
