6334136fd7929f4193e356c294e3351f842c4648
Brenda-M <brendanjuguna1@gmail.com>
2020-03-04 15:31:41 +0300

    link movement html and js

--- js/mock_data.js
+++ js/mock_data.js
@@ -13,12 +13,53 @@ const Movements = [
     {
         title: "My mental health",
         description: "On trauma and depression, resilience and strength...",
-        detailedDescription: ""
+        detailedDescription:  `Right now it is estimated that up to 12 million metric tons of plastic—everything
+        from plastic bottles and bags to microbeads—end up in our oceans each year. That’s a truckload of trash every minute.
+        Traveling on ocean currents, this plastic is now turning up in every corner of our planet, from Florida beaches to 
+        uninhabited Pacific islands.It is even being found in the deepest part of the ocean and trapped in Arctic ice.
+        Our oceans are slowly turning into a plastic soup, and the effects on ocean life are devastating.Plastic pieces of all 
+        sizes choke and clog the stomachs of creatures who mistake it for food, from tiny zooplankton to whales.
+        Plastic is now entering every level of the ocean food chain and is even ending up in the seafood on our plates.
+        But it doesn’t have to be this way.That’s why we are campaigning to end the flow of single- use plastic into our oceans.
+        We are calling on big corporations to act to reduce their plastic footprint—and stop producing plastic packaging that is 
+        designed to be used for just a few minutes before it ends up in landfills, incinerators and out polluting our environment for a lifetime.
+        We’re also working hard to address other serious threats facing our oceans.Unsustainable industrial fishing is destroying habitats and 
+        endangering countless species.Climate change and ocean acidification—both the result of our reliance on fossil fuels—are having more and 
+        more extreme impacts on ocean health.Today, overfishing and bycatch kills about 63 billion pounds of marine animals every year, and human 
+        activity is disrupting the balance of marine ecosystems across the globe.The impacts on humans are equally severe.Overfishing compromises 
+        food security and the livelihoods of fishing communities.Human trafficking and forced labor remain huge problems on many fishing fleets.
+        We’re also working to protect the oceans through a network of sanctuaries.Globally, less than 2 percent of the ocean is under protection.
+        We’re campaigning to establish ocean sanctuaries in 30 percent of the world’s oceans by 2030.These sanctuaries will preserve biodiversity, 
+        help endangered species recover, and give marine life a fighting chance to survive the rapid changes we are causing to the planet.
+        Ocean sanctuaries can also help replenish fish populations decimated by overfishing, meaning a more dependable food supply for the billions
+        of people who get some of their protein from seafood.Scientists say the wave of extinction facing the ocean in the coming century could be the worst 
+        since the dinosaur age.If we don’t change the way we do things, and fast, we are on track to cause irreversible damage to the ocean and the collapse of 
+        some of the most important food sources in the world.Fortunately, if we work together we are within reach of a world that respects our oceans, their 
+        inhabitants and the people who depend on them.We want a better future for our oceans and the people that depend on them.You do, too ? Awesome! Check out 
+        the resources below to learn more about our campaign to protect the oceans and ways you can get involved!`,
         image: "assets/recents2.jpeg"
     },
     {
         title: "Tackel Hunger",
         description: "Collect food items for your local food bank",
+        detailedDescription:`Our democracy should represent the interests of the people — 
+        interests like a safe climate and healthy environment.Yet the combination of corporate 
+        money in politics, barriers to voting for many, skewed rules around gerrymandering, and a 
+        recent crackdown on peaceful protest has rendered our democracy ill-equipped to advance the 
+        public good More than 60% of the American public believes that climate change is real and to some extent,
+         manmade, and yet Congress’s inaction on this crisis exposes just how little our government is currently 
+         representing the people, on this and on other key issues. Communities across the U.S. are dealing with 
+         the devastating impacts of climate change and it should be easy to pass laws that cut carbon pollution,
+        propel renewable energy, and support vulnerable communities. Yet, we see politicians block reform on climate 
+        change time and again and the reason why is simple — the fossil fuel industry is spending millions of dollars 
+        on politicians who will support their bottom line on Capitol Hill.This hold on our democracy is blocking communities
+         access to clean energy. It’s contributing to a growing gap between the rich and the poor and it’s making it harder for 
+         marginalized communities to vote. It’s allowing companies to control what we eat and it’s hindering clean water protection 
+         in towns all over the country.We are ready for a government accountable to all people, not just a select few, and we are 
+         determined to fight for a democracy that shifts power back into the hands of people so together we can make a more just, 
+         equitable world for all. We are ready to march on Capitol Hill, get on the phone with representatives, take to the streets 
+         in their communities, participate in creative protest, and build a new future together.Together, we can fight for a democracy 
+         that is truly of, by, and for the people.`,
         image: "assets/recents3.jpeg"
     },
     {
@@ -35,17 +76,52 @@ const Movements = [
     {
         title: "Ending the climate crisis",
         description: "We are in a climate crisis emergency and the oil, gas, and coal indutry is to blame..",
+        detailedDescription: `Our climate is in crisis. Scientists are shouting an urgent warning: we have little 
+        more than a decade to take bold, ambitious action to transition our economy off of coal, oil, and gas, and 
+        onto safe and green renewable energy. Shifting to cleaner, locally-run energy will not only slow the tide 
+        of climate catastrophe, it’ll create millions of new jobs that sustain families while protecting community health.
+        For the last decade, many elected leaders and corporations have claimed to care about our future, but have done little 
+        more than say they believe climate change is real. Those talking points don’t cut it anymore.It’s time to put people 
+        above the fossil fuel industry and truly act on climate.With enough pressure, we can push leaders to start responsibly phasing 
+        out coal, oil, and gas while prioritizing support for disadvantaged communities and workers most affected by moving to a safer, 
+        more caring economy. Together we can also make sure that communities impacted by extreme climate impacts will have everything they 
+        need to recover in a way that supports sustainable and liveable communities, and holds fossil fuel executives accountable. True climate 
+        action also means that giant multinational corporations like Amazon need to invest in locally-run renewable energy to power their operations 
+        instead of dirty, dangerous energy production.We deserve a world where everyone can thrive without worrying about their health, finances, or pollution.`,
         image: "assets/recents6.jpeg"
     },
     {
         title: "Protecting the oceans",
         description: "Our oceans are in more trouble than ever before.",
-        detailedDescription: "",
+        detailedDescription:   `Our oceans are in more trouble than ever before.Right now it is estimated that up to 
+        12 million metric tons of plastic—everything from plastic bottles and bags to microbeads—end up in our oceans each year. 
+        That’s a truckload of trash every minute.Traveling on ocean currents, this plastic is now turning up in every corner of 
+        our planet, from Florida beaches to uninhabited Pacific islands. It is even being found in the deepest part of the ocean and 
+        trapped in Arctic ice.Our oceans are slowly turning into a plastic soup, and the effects on ocean life are devastating. 
+        Plastic pieces of all sizes choke and clog the stomachs of creatures who mistake it for food, from tiny zooplankton to whales. 
+        Plastic is now entering every level of the ocean food chain and is even ending up in the seafood on our plates.But it doesn’t 
+        have to be this way. That’s why we are campaigning to end the flow of single-use plastic into our oceans.We are calling on big 
+        corporations to act to reduce their plastic footprint—and stop producing plastic packaging that is designed to be used for just a 
+        few minutes before it ends up in landfills, incinerators and out polluting our environment for a lifetime.We’re also working hard to 
+        address other serious threats facing our oceans. Unsustainable industrial fishing is destroying habitats and endangering countless species. 
+        Climate change and ocean acidification—both the result of our reliance on fossil fuels—are having more and more extreme impacts on ocean health.
+         Today, overfishing and bycatch kills about 63 billion pounds of marine animals every year, and human activity is disrupting the balance of marine 
+         ecosystems across the globe. The impacts on humans are equally severe. Overfishing compromises food security and the livelihoods of fishing communities. 
+         Human trafficking and forced labor remain huge problems on many fishing fleets.We’re also working to protect the oceans through a network of sanctuaries. 
+        Globally, less than 2 percent of the ocean is under protection. We’re campaigning to establish ocean sanctuaries in 30 percent of the world’s oceans by 2030.
+        These sanctuaries will preserve biodiversity, help endangered species recover, and give marine life a fighting chance to survive the rapid changes we are causing 
+        to the planet. Ocean sanctuaries can also help replenish fish populations decimated by overfishing, meaning a more dependable food supply for the billions of 
+        people who get some of their protein from seafood.Scientists say the wave of extinction facing the ocean in the coming century could be the worst since the dinosaur 
+        age. If we don’t change the way we do things, and fast, we are on track to cause irreversible damage to the ocean and the collapse of some of the most important
+         food sources in the world.Fortunately, if we work together we are within reach of a world that respects our oceans, their inhabitants and the people who depend 
+         on them.We want a better future for our oceans and the people that depend on them. You do, too? Awesome! Check out the resources below to learn more about our 
+         campaign to protect the oceans and ways you can get involved!`,
         image: "assets/recents7.jpeg"
     },
     {
         title: "Defending Democracy",
         description: "We believe that a healthy democracy is the precondition to a healthy environment...",
+        
         image: "assets/recents8.jpeg"
     },
     {
